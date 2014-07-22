fb3f6a453f1357e5d74c1ebc60b9d56f5fe2980d
jejacobs <jejacobs@adobe.com>
2014-07-21 18:16:18 -0700

    continued implementing conditionals- need relative position detection

--- js/src/main.js
+++ js/src/main.js
@@ -3,46 +3,55 @@
 require.config({
 
     // Setup paths so our require.js text plugin can be resolved.
-    baseUrl: "js/src",
+    baseUrl: 'js/src',
     // Boost module timeout slightly for slower machines.
 	paths : {   
-        "jquery" : "../lib/jquery/jquery", //specific libraries -- can be specified later
-        "paper" : "../lib/paper/dist/paper",
-        "backbone" : "../lib/backbone/backbone",
-        "underscore" : "../lib/underscore/underscore",
-        "handlebars"  : "../lib/handlebars/handlebars",
-        "toolbox": "../lib/toolbox/toolbox"
+        'jquery' : '../lib/jquery/jquery', //specific libraries -- can be specified later
+        'paper' : '../lib/paper/dist/paper',
+        'backbone' : '../lib/backbone/backbone',
+        'underscore' : '../lib/underscore/underscore',
+        'handlebars'  : '../lib/handlebars/handlebars',
+        'toolbox': '../lib/toolbox/toolbox',
+        'justmath': '../lib/justmath/justmath',
+        'sylvester': '../lib/sylvester/sylvester'
     },
     
     shim: {
         paper : {
-            exports: "paper"
+            exports: 'paper'
         },
         backbone: {
-        deps: ["underscore", "jquery"],
-        exports: "Backbone"
+        deps: ['underscore', 'jquery'],
+        exports: 'Backbone'
         },
 
         underscore: {
-         exports: "_"
+         exports: '_'
         },
 
         handlebars: {
-         exports: "Handlebars"
+         exports: 'Handlebars'
         },
         toolbox:{
-          exports: "Toolbox"
+          exports: 'Toolbox'
+        },
+        justmath:{
+          exports:'JustMath'
+        },
+        sylvester:{
+          exports:'Sylvester'
         }
+       
     },
 });
 
 require([
   // Load our app module and pass it to our definition function
-  "app",
+  'app',
 
 ], function(App){
-  // The "app" dependency is passed in as "App"
-  // Again, the other dependencies passed in are not "AMD" therefore don't pass a parameter to this function
+  // The 'app' dependency is passed in as 'App'
+  // Again, the other dependencies passed in are not 'AMD' therefore don't pass a parameter to this function
   App.initialize();
 });
 
